fb31b350ccb4aaae254e3c7017b879654b1c66c9
vishnu <vishnuviswambara2002@gmail.com>
2026-07-04 15:12:11 +0000

    chore: settings page

---
+++ nextgenie/nextgenie/doctype/nextgenie_settings/nextgenie_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Codeventurers and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Nextgenie Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
