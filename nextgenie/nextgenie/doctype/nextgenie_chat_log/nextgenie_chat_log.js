ec45a491aef1bfa27a2ae1689c9b6fbcb6981a8e
vishnu <vishnuviswambara2002@gmail.com>
2026-07-04 07:27:54 +0000

    chore: ui corrections

--- nextgenie/nextgenie/doctype/nextgenie_chat_log/nextgenie_chat_log.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Codeventurers and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("NextGenie Chat Log", {
-// 	refresh(frm) {
-
-// 	},
-// });
